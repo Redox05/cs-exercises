62150ece597e332e3dbe53d5f0d15a02466c0329
RBN <iq.rubenop@gmail.com>
2025-03-14 23:42:14 -0600

    Print table of guests and total of guests.

--- ConsoleUI/MiniProject/Program.cs
+++ ConsoleUI/MiniProject/Program.cs
@@ -10,49 +10,73 @@
             string firstName = "";
             int partyMembers = 0;
             string keyWord = "";
+
             do
             {
+            A_label:
                 firstName = GetUserInfo("Your name: ");
-                partyMembers = StringToInt(GetUserInfo("#Party: "));
+                partyMembers = StringToInt(GetUserInfo("# Party Members: "));
 
-                guestBook.Add(firstName, partyMembers);
+                if (partyMembers>0)
+                {
+                    guestBook.Add(firstName, partyMembers);
+                }
+                else
+                {
+                    WriteLine("That is not a valid party number. Try again.");
+                    goto A_label;
+                }
 
                 Write("Are ther any more guests? yes/no ");
                 keyWord = ReadLine();
+                WriteLine();
 
             } while (keyWord.ToLower() == "yes");
 
             WriteLine("{0,-12} {1,15}", arg0: "Guest", "# Party Members");
 
-            foreach (var name in guestBook)
+            PrintGuestTable(guestBook);
+
+            WriteLine($"Total number of guests: {TotalGuests(guestBook)}");
+
+        }
+
+        private static void PrintGuestTable(Dictionary<string,int> book)
+        {
+            foreach (var name in book)
             {
-                WriteLine();
+                WriteLine($"{name.Key,-12} {book[name.Key],15}");
             }
-
-
-            
-
-            string GetUserInfo(string message)
+        }
+        private static int TotalGuests(Dictionary<string,int> book)
+        {
+            int total = 0;
+            foreach (var name in book)
             {
-                Write(message);
-                string outputText = ReadLine();
-
-                return outputText;
+                total += name.Value;
             }
+            return total;
+        }
+        private static string GetUserInfo(string message)
+        {
+            Write(message);
+            string outputText = ReadLine();
 
-            int StringToInt(string input)
+            return outputText;
+        }
+
+        private static int StringToInt(string input)
+        {
+            int returnValue;
+            bool isValidInt = int.TryParse(input, out int output);
+            if (isValidInt == true)
             {
-                int returnValue;
-                bool isValidInt = int.TryParse(input, out int output);
-                if (isValidInt == true)
-                {
-                    returnValue = output;
-                    return returnValue;
-                }
-                else
-                {
-                    return -1;
-                }
+                returnValue = output;
+                return returnValue;
+            }
+            else
+            {
+                return -1;
             }
         }
     }
