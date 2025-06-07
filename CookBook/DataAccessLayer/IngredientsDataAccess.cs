fab39405a0da527d1deeeb51b1cb9603b602831d
RBN <iq.rubenop@gmail.com>
2025-06-07 06:07:31 -0600

    Change stored procedure for inline query

--- CookBook/DataAccessLayer/IngredientsDataAccess.cs
+++ CookBook/DataAccessLayer/IngredientsDataAccess.cs
@@ -13,13 +13,15 @@ namespace DataAccessLayer
 {
     public class IngredientsDataAccess
     {
-        public void AddIngredient(Ingredient ingredient)
+        public void AddIngredient(Ingredient  ingredient)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["CookBookConnectionString"].ConnectionString;
 
+            string query = "insert into Ingredients (Name, Weight, KcalPer100g, PricePer100g, Type) values(@Name, @Weight, @KcalPer100g, @PricePer100g, @Type)";
+
             using (IDbConnection connection = new SqlConnection(connectionString))
             {
-                connection.Execute("dbo.InsertIngredientProcedure @Name, @Weight, @KcalPer100g, @PricePer100g, @Type", ingredient);
+                connection.Execute(query, ingredient);
             }
         }
     }
