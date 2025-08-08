29ef875a3a7b91ba5a6351bffefe639d1c3bea95
RBN <iq.rubenop@gmail.com>
2025-08-08 13:35:16 -0600

    Create ConnectionHelper class, again.

--- CookBook/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ CookBook/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746d8a733b9fcfa5fb5e85330ed7994e131d7d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528f4bff9d7a88fac8cb782367c18fbba06fd857")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
