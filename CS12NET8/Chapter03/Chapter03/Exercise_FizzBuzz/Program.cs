23a30cd6c3cbfd7a74fc2108a2f45460c0f78048
RBN <iq.rubenop@gmail.com>
2026-06-16 21:47:38 -0600

    FizzBuzz exercise

---
+++ CS12NET8/Chapter03/Chapter03/Exercise_FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿int counter = 100;
+
+for (int i = 1; i<=counter; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("FizzBuzz ");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("Fizz ");
+    }
+    else if(i % 5 == 0 )
+    {
+        Console.Write("Buzz ");
+    }
+    else
+    {
+        Console.Write(i + " ");
+    }
+}
