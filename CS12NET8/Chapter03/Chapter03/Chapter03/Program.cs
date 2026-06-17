652dabc397c174b2744e711c0c346631c7706217
RBN <iq.rubenop@gmail.com>
2026-06-16 21:25:05 -0600

    Loops and overflow exercise.

---
+++ CS12NET8/Chapter03/Chapter03/Chapter03/Program.cs
@@ -0,0 +1,13 @@
+﻿int max = 500;
+for(byte i=0; i<max; i++)
+{
+    if(max > byte.MaxValue)
+    {
+        Console.WriteLine("The value of max exceeds the max value of byte. Cannot continue ");
+        return;
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
